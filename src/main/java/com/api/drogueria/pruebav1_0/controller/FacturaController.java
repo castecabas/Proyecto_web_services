17c3f4dcf19ae12feade475bb371aa906b5fd029
Carde <Carde@DESKTOP-VCBF6KQ>
2023-05-24 12:31:02 -0500

    v2.0.1

--- src/main/java/com/api/drogueria/pruebav1_0/controller/FacturaController.java
+++ src/main/java/com/api/drogueria/pruebav1_0/controller/FacturaController.java
@@ -5,17 +5,13 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.api.drogueria.pruebav1_0.model.Factura;
 import com.api.drogueria.pruebav1_0.model.Producto;
 import com.api.drogueria.pruebav1_0.repository.ProductoRepository;
 import com.api.drogueria.pruebav1_0.service.ClienteService;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class FacturaController {
