8dbf04dd92a520c071f0b6bda5a031bf5c42fd84
castecabas <124809053+castecabas@users.noreply.github.com>
2023-05-29 14:26:12 -0500

    vn1.2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,6 +8,21 @@ INSERT INTO regiones (id, nombre) VALUES (6, 'Africa');
 INSERT INTO regiones (id, nombre) VALUES (7, 'Oceanía');
 INSERT INTO regiones (id, nombre) VALUES (8, 'Antártida');
 
+/* CLIENTES */
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(1,'Camilo', 'Guzmán', 'profesor@gmail.com', '2018-01-01');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(2,'Mr. John', 'Doe', 'john.doe@gmail.com', '2018-01-02');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(1,'Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2018-01-03');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(3,'Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2018-01-04');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(4,'Erich', 'Gamma', 'erich.gamma@gmail.com', '2018-02-01');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(4,'Richard', 'Helm', 'richard.helm@gmail.com', '2018-02-10');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(2,'Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2018-02-18');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(1,'John', 'Vlissides', 'john.vlissides@gmail.com', '2018-02-28');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(5,'Dr. James', 'Gosling', 'james.gosling@gmail.com', '2018-03-03');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(6,'Magma', 'Lee', 'magma.lee@gmail.com', '2018-03-04');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(7,'Tornado', 'Roe', 'tornado.roe@gmail.com', '2018-03-05');
+INSERT INTO clientes (region_id,nombre, apellido, correo, create_at) VALUES(8,'Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06');
+
+
 /* PRODUCTOS */
 INSERT INTO productos (Nombre,Precio,create_at) VALUES ('Val',500,'2020-05-04');
 INSERT INTO productos (Nombre,Precio,create_at) VALUES ('H&S',1200,NOW());
